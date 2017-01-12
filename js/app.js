09308c3ad6964447c05f161704a4089d999d3eae
Keith Kelly <keithkelly31@gmail.com>
2017-01-11 21:35:13 -0500

    Set up Firebase Hosting
    Added sorting buttons and set up Firebase hosting

--- js/app.js
+++ js/app.js
@@ -8,30 +8,45 @@
     messagingSenderId: "712478989965"
   });
 
-  firebase.database().ref('cheesecakes').orderByChild('price').on('value', function(cheesecakes) {
-    cheesecakes.forEach(function(cheesecake) {
-      const buttonId = cheesecake.val().name.toLowerCase().replace(' ', '-');
-      html = `
-        <div class="cheesecake">
-          <img src="${cheesecake.val().img}" alt="${cheesecake.val().name}">
-          <h3>${cheesecake.val().name}</h3>
-          <div class="info">
-            <p>${cheesecake.val().desc}</p>
-            <div class="order">
-              <span>$${cheesecake.val().price}</span>
-              <button id="${buttonId}">Order</button>
+  function getCheesecakes(sort) {
+    firebase.database().ref('cheesecakes').orderByChild(sort).on('value', function(cheesecakes) {
+      // Remove any previous results
+      document.getElementById('cheesecakes').innerHTML = '';
+
+      cheesecakes.forEach(function(cheesecake) {
+        const buttonId = cheesecake.val().name.toLowerCase().replace(' ', '-');
+        html = `
+          <div class="cheesecake">
+            <img src="${cheesecake.val().img}" alt="${cheesecake.val().name}">
+            <h3>${cheesecake.val().name}</h3>
+            <div class="info">
+              <p>${cheesecake.val().desc}</p>
+              <div class="order">
+                <span>$${cheesecake.val().price}</span>
+                <button id="${buttonId}">Order</button>
+              </div>
             </div>
           </div>
-        </div>
-      `;
+        `;
 
-      document.getElementById('cheesecakes').insertAdjacentHTML('beforeend', html);
-      document.getElementById(buttonId).addEventListener('click', function() {
-        const name = cheesecake.val().name;
-        const subject = encodeURIComponent('Order ' + name + ' Cheesecake');
-        const body = encodeURIComponent('Hi Jon,\n\nI would like to order the ' + name + ' cheesecake.\n\n{{Please insert any details or quetions you have here}}');
-        window.open('mailto:jon_hernandez@msn.com?subject=' + subject + '&body=' + body);
+        document.getElementById('cheesecakes').insertAdjacentHTML('beforeend', html);
+        document.getElementById(buttonId).addEventListener('click', function() {
+          const name = cheesecake.val().name;
+          const subject = encodeURIComponent('Order ' + name + ' Cheesecake');
+          const body = encodeURIComponent('Hi Jon,\n\nI would like to order the ' + name + ' cheesecake.\n\n{{Please insert any details or quetions you have here}}');
+          window.open('mailto:jon_hernandez@msn.com?subject=' + subject + '&body=' + body);
+        });
       });
     });
+  }
+
+  document.getElementById('sort-name').addEventListener('click', function() {
+    getCheesecakes('name');
   });
+
+  document.getElementById('sort-price').addEventListener('click', function() {
+    getCheesecakes('price');
+  });
+
+  getCheesecakes('name');
 }();
